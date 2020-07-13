2cbbaeaf80fbad00ac16d429fe2d23446b0aec50
jntakpe <jntakpe@gmail.com>
2020-07-13 22:09:15 +0200

    feat: use GRPC reactor plugin

--- src/main/kotlin/com/github/jntakpe/users/endpoints/UsersEndpoint.kt
+++ src/main/kotlin/com/github/jntakpe/users/endpoints/UsersEndpoint.kt
@@ -1,33 +1,31 @@
 package com.github.jntakpe.users.endpoints
 
+import com.github.jntakpe.users.ReactorUsersServiceGrpc
 import com.github.jntakpe.users.Users
-import com.github.jntakpe.users.UsersServiceGrpc
 import com.github.jntakpe.users.mappings.toEntity
 import com.github.jntakpe.users.mappings.toResponse
 import com.github.jntakpe.users.service.UserService
-import io.grpc.stub.StreamObserver
+import reactor.core.publisher.Mono
 import javax.inject.Singleton
 
 @Singleton
-class UsersEndpoint(private val userService: UserService) : UsersServiceGrpc.UsersServiceImplBase() {
+class UsersEndpoint(private val userService: UserService) : ReactorUsersServiceGrpc.UsersServiceImplBase() {
 
-    override fun findByUsername(request: Users.UsersByUsernameRequest, responseObserver: StreamObserver<Users.UserResponse>) {
-        val user = with(Users.UserResponse.newBuilder()) {
-            username = "jntakpe"
-            email = "jntakpe@mail.com"
-            lastName = "N'takpé"
-            build()
-        }
-        responseObserver.onNext(user)
-        responseObserver.onCompleted()
+    override fun findByUsername(request: Mono<Users.UsersByUsernameRequest>): Mono<Users.UserResponse> {
+        return request
+                .map {
+                    with(Users.UserResponse.newBuilder()) {
+                        username = "jntakpe"
+                        email = "jntakpe@mail.com"
+                        lastName = "N'takpé"
+                        build()
+                    }
+                }
     }
 
-    override fun create(request: Users.UserRequest, responseObserver: StreamObserver<Users.UserResponse>) {
-        userService.create(request.toEntity())
-                .doOnNext {
-                    responseObserver.onNext(it.toResponse())
-                    responseObserver.onCompleted()
-                }
-                .subscribe()
+    override fun create(request: Mono<Users.UserRequest>): Mono<Users.UserResponse> {
+        return request
+                .flatMap { userService.create(it.toEntity()) }
+                .map { it.toResponse() }
     }
 }
