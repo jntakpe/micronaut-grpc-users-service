155412c307f3f316f975b5c8a302ec17cbbaa9c2
jntakpe <jntakpe@gmail.com>
2020-11-08 12:35:02 +0100

    feat: bump to commons 0.1.7, to kmongo 4.2.0 and update repository accordingly

--- src/main/kotlin/com/github/jntakpe/users/repository/UserRepository.kt
+++ src/main/kotlin/com/github/jntakpe/users/repository/UserRepository.kt
@@ -9,11 +9,10 @@ import com.mongodb.reactor.client.toReactor
 import org.bson.types.ObjectId
 import org.litote.kmongo.ascending
 import org.litote.kmongo.eq
-import org.litote.kmongo.reactivestreams.findOne
-import org.litote.kmongo.reactivestreams.findOneById
 import org.litote.kmongo.reactivestreams.getCollection
+import org.litote.kmongo.reactor.findOne
+import org.litote.kmongo.reactor.findOneById
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 import javax.inject.Singleton
 
 @Singleton
@@ -22,13 +21,13 @@ class UserRepository(database: MongoDatabase) {
     private val collection = database.getCollection<User>().toReactor()
 
     init {
-        collection.createIndex(ascending(Username), IndexOptions().unique(true)).toMono().subscribe()
-        collection.createIndex(ascending(Email), IndexOptions().unique(true)).toMono().subscribe()
+        collection.createIndex(ascending(Username), IndexOptions().unique(true)).subscribe()
+        collection.createIndex(ascending(Email), IndexOptions().unique(true)).subscribe()
     }
 
-    fun findById(id: ObjectId): Mono<User> = collection.findOneById(id).toMono()
+    fun findById(id: ObjectId): Mono<User> = collection.findOneById(id)
 
-    fun findByUsername(username: String): Mono<User> = collection.findOne(Username eq username).toMono()
+    fun findByUsername(username: String): Mono<User> = collection.findOne(Username eq username)
 
     fun create(user: User): Mono<User> = collection.insertOne(user).thenReturn(user)
 }
