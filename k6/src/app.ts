74f9627a9a784ce25dbf3b8f9bbc49e29ade984f
jntakpe <jntakpe@gmail.com>
2020-11-17 22:43:35 +0100

    feat: init K6 application that creates an user and query it using both find by id and find by username endpoints

---
+++ k6/src/app.ts
@@ -0,0 +1,72 @@
+import { group } from 'k6';
+import { ExtendedOptions, profileConfig } from './config/profile-config';
+import { UsersApi } from './apis/users-api';
+import { checkInterval, rangeVuIterSuffix } from './helpers/utils';
+import { conflictResponseChecks, createResponseChecks, notFoundResponseChecks, okResponseChecks } from './helpers/checks';
+import { isOk } from './helpers/assertions';
+import { UsersGrpcApi } from './apis/users-grpc-api';
+
+const options = profileConfig();
+const userTemplate = {
+    'username': 'k6test',
+    'email': 'k6test@mail.com',
+    'first_name': 'K6',
+    'last_name': 'Test',
+    'phone_number': '123456789',
+    'country_code': 'FR'
+}
+
+export default () => {
+    userLifecycle(contextualizeUser(userTemplate), options);
+}
+
+function userLifecycle(user, options: ExtendedOptions) {
+    const interval = options.callInterval;
+    const userApi = new UsersGrpcApi();
+    //or const userApi = new UsersRestApi();
+    return group(`User lifecycle`, () => {
+        const createdUser = userCreation(userApi, user, interval);
+        if (createdUser) {
+            userRetrieval(userApi, createdUser, interval)
+            return createdUser.id;
+        }
+    });
+}
+
+function userCreation(userApi: UsersApi, user, interval: number) {
+    return group('User creation', () => {
+        const created = group('New user creation', () => {
+            const response = userApi.create(user);
+            checkInterval(interval, response, createResponseChecks);
+            if (isOk(response)) {
+                const body = response.message || response.json();
+                return body.id && body;
+            }
+        });
+        if (created) {
+            group('Already existing user creation', () => {
+                checkInterval(interval, userApi.create(user), conflictResponseChecks);
+            });
+        }
+        return created;
+    });
+}
+
+function userRetrieval(userApi: UsersApi, user, interval: number) {
+    return group('User retrieval', () => {
+        group('Find user by id', () => {
+            checkInterval(interval, userApi.findById(user.id), okResponseChecks(user.id));
+        });
+        group('Error when by does not exists', () => {
+            checkInterval(interval, userApi.findById('123456789012345678901234'), notFoundResponseChecks);
+        });
+        group('Find user by username', () => {
+            checkInterval(interval, userApi.findByUsername(user.username), okResponseChecks(user.id));
+        });
+    })
+}
+
+function contextualizeUser(user, suffix = rangeVuIterSuffix()) {
+    const username = `${user.username}${suffix}`
+    return {...user, username, email: `${username}@mail.com`}
+}
