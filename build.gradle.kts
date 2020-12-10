cb32396bf0e1d5f2ce3e450c06e6d4c96b7c00de
jntakpe <jntakpe@gmail.com>
2020-12-10 22:22:55 +0100

    build: bump jib plugin to 2.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion
     id("com.google.protobuf") version "0.8.13"
     id("io.micronaut.application") version "1.2.0"
-    id("com.google.cloud.tools.jib") version "2.6.0"
+    id("com.google.cloud.tools.jib") version "2.7.0"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
